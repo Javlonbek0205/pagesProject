6f096638b5354571b9d3fe52d713f81ea195a3cf
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2024-07-05 16:59:59 +0500

    herokuga moslandi

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m2bq&=esluhg852#fb&fjktdx=#la0mebr(61p)w40zp^=#-)#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
